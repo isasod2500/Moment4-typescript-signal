94026ada22541aa5d141a8deb7adf83dfddcb01a
isso2500 <isso2500@student.miun.se>
2026-05-04 11:38:48 +0200

    Sorting implemented. Depending on ID of element pressed, function sorts accordingly. Variable "sorted" is created to allow ascending(=true) and descending(=false (after clicking twice) sorting.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -12,8 +12,10 @@ import { CommonModule } from '@angular/common';
 export class Home {
   courses = signal<Course[]>([]);
   error = signal<string | null>(null);
+  sorted = false;
 
   courseService = inject(courseService);
+
 
   ngOnInit() {
     this.loadCourses();
@@ -23,9 +25,60 @@ export class Home {
     try {
       const response = await this.courseService.fetchCourses();
       this.courses.set(response)
-    } catch(error) {
+    } catch (error) {
       console.error(error);
       this.error.set("Kunde inte ladda data - försök igen senare");
     }
+  }
+
+  findID(id: Event) {
+    const element = id.target as HTMLElement
+    this.filterCourses(element.id)
+  }
+
+  //Filtrerar kurskod, namn och prog.
+  async filterCourses(id: string) {
+    try {
+      const response = await this.courseService.fetchCourses();
+
+      if (id === "code") {
+        if (this.sorted == false) {
+          response.sort((a, b) => a.code.localeCompare(b.code))
+          this.courses.set(response)
+          this.sorted = true;
+        } else {
+          response.sort((a, b) => b.code.localeCompare(a.code))
+          this.courses.set(response)
+          this.sorted = false;
+        }
+      }
+
+      if (id === "name") {
+        if (this.sorted == false) {
+          response.sort((a, b) => a.coursename.localeCompare(b.coursename))
+          this.courses.set(response)
+          this.sorted = true;
+        } else {
+          response.sort((a, b) => b.coursename.localeCompare(a.coursename))
+          this.courses.set(response)
+          this.sorted = false;
+        }
+      }
+
+      if (id === "progression") {
+        if (this.sorted == false) {
+          response.sort((a, b) => a.progression.localeCompare(b.progression))
+          this.courses.set(response)
+          this.sorted = true;
+        } else {
+          response.sort((a, b) => b.progression.localeCompare(a.progression))
+          this.courses.set(response)
+          this.sorted = false;
+        }
+      }
+    } catch (err) {
+      console.log(err)
+    }
+
   }
 }
